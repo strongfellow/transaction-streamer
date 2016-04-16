4faba14b50975ec90785a18e5ae081efd9530ef3
strongfellow <a@b.com>
2016-04-15 17:46:40 -0700

    added a logging statement

--- src/main/java/com/strongfellow/transactions/stomp/GreetingController.java
+++ src/main/java/com/strongfellow/transactions/stomp/GreetingController.java
@@ -1,5 +1,8 @@
 package com.strongfellow.transactions.stomp;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
@@ -7,11 +10,13 @@ import org.springframework.stereotype.Controller;
 @Controller
 public class GreetingController {
 
+    private static final Logger logger = LoggerFactory.getLogger(GreetingController.class);
 
     @MessageMapping("/hello")
-    @SendTo("/topic/greetings")
+    @SendTo("/topic/greet")
     public Greeting greeting(HelloMessage message) throws Exception {
-        Thread.sleep(3000); // simulated delay
+        logger.info("greeting requested");
+        Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + message.getName() + "!");
     }
 
