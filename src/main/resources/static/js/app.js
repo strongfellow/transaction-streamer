8a4983c1ed531bce34b48bdee906a3cecf02bd23
Strongfellow <a@b.com>
2016-04-23 22:05:44 -0700

    whitespace changes; this works well

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,47 +1,39 @@
 
 require([ "sockjs-0.3.4", "stomp", "jquery", "jquery-ui/autocomplete" ], function( sock, stomp, $ ) {
-  $("<p>").html("jquery loaded").appendTo("body");
-  $( "<input>" )
-    .autocomplete({ source: [ "One", "Two", "Three" ]})
-    .appendTo( "body" );
+    $("<p>").html("jquery loaded").appendTo("body");
 
     $("#connect").click(connect);
     $("#disconnect").click(disconnect);
     
-  var stompClient = null;
+    var stompClient = null;
         
-  function setConnected(connected) {
-    $('#connect').disabled = connected;
-    $('#disconnect').disabled = !connected;
-  }
-
-  function connect() {
-    var socket = new SockJS('/stomp');
-    stompClient = Stomp.over(socket);            
-    stompClient.connect({}, function(frame) {
-      setConnected(true);
-      console.log('Connected: ' + frame);
-	stompClient.subscribe('/topic/transactions', function(greeting){
-	    console.log(greeting.body);
-//        showGreeting(JSON.parse(greeting.body).content);
-      });
-    });
-  }
-  
-  function disconnect() {
-    if (stompClient != null) {
-      stompClient.disconnect();
+    function setConnected(connected) {
+	$('#connect').disabled = connected;
+	$('#disconnect').disabled = !connected;
     }
-    setConnected(false);
-    console.log("Disconnected");
-  }
+
+    function appendTx(message) {
+	console.log(message.body);
+    }
+    
+    function connect() {
+	var socket = new SockJS('/stomp');
+	stompClient = Stomp.over(socket);            
+	stompClient.connect({}, function(frame) {
+	    setConnected(true);
+	    console.log('Connected: ' + frame);
+	    stompClient.subscribe('/topic/transactions', function(message) {
+		appendTx(message);
+	    });
+	});
+    }
   
-  function sendName() {
-    var name = $('#name').value;
-    stompClient.send("/app/hello", {}, JSON.stringify({ 'name': name }));
-  }
-        
-  function showGreeting(message) {
-    $('<p>').html(message).appendTo($('#response'));
-  }
+    function disconnect() {
+	if (stompClient != null) {
+	    stompClient.disconnect();
+	}
+	setConnected(false);
+	console.log("Disconnected");
+    }
+  
 });
