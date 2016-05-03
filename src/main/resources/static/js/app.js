0076672c4f93de37d5456872c6ed5f213ce516f4
strongfellow <a@b.com>
2016-05-02 22:23:42 -0700

    connect upon page load

--- src/main/resources/static/js/app.js
+++ src/main/resources/static/js/app.js
@@ -1,42 +1,33 @@
 
 require([ "sockjs-0.3.4", "stomp", "jquery", "jquery-ui/autocomplete" ], function( sock, stomp, $ ) {
 
-    $("#connect").click(connect);
-    $("#disconnect").click(disconnect);
-    
     var stompClient = null;
-        
-    function setConnected(connected) {
-	$('#connect').disabled = connected;
-	$('#disconnect').disabled = !connected;
-    }
 
     function appendTx(message) {
         var p = $('<p>').html(message.body).hide().addClass('tx');
 	    $('#transactions-list').prepend(p);
-	    $('#transactions-list p').slice(20).remove();        
+	    $('#transactions-list p').slice(20).remove();
 	    p.slideDown(400, function() {
 	    });
     }
-    
+
     function connect() {
 	var socket = new SockJS('/stomp');
-	stompClient = Stomp.over(socket);            
+	stompClient = Stomp.over(socket);
 	stompClient.connect({}, function(frame) {
-	    setConnected(true);
 	    console.log('Connected: ' + frame);
 	    stompClient.subscribe('/topic/transactions', function(message) {
 		appendTx(message);
 	    });
 	});
     }
-  
+
     function disconnect() {
 	if (stompClient != null) {
 	    stompClient.disconnect();
 	}
-	setConnected(false);
 	console.log("Disconnected");
     }
-  
+
+    $(function() { connect(); });
 });
